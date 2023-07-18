57a9aaf965aa948cb29de93997f9f021c35f41b2
sajid <mohdsajid8010@gmail.com>
2023-07-18 21:02:28 +0530

    commit1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [inputValue, setinputValue] = useState("")
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="parent">
+      {/* Do not remove the main div */}
+      <h1>Parent Component</h1>
+
+      <div>{inputValue}</div>
+      <div className="child">
+        <h1>Child Component</h1>
+        <input onChange={(e) => setinputValue(e.target.value)} />
+      </div>
     </div>
   )
 }
